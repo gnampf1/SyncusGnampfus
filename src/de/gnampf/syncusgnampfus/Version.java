af2ded8b55e75a4d3a01d30fb96e9c097ec208ec
Daniel Ridders <dridders-github@dridders.de>
2025-09-09 20:02:30 +0200

    Plugin Version 0.10.0

--- src/de/gnampf/syncusgnampfus/Version.java
+++ src/de/gnampf/syncusgnampfus/Version.java
@@ -3,5 +3,5 @@ package de.gnampf.syncusgnampfus;
 public class Version 
 {
 	public final static String BUILD = "BUILD";
-	public final static String VERSION = "0.9";
+	public final static String VERSION = "0.10";
 }
